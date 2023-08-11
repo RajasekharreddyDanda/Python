e17559ed72d78c0ccbca5a8c541f62e8eee9ccbe
RajDanda <rajasekharreddy.danda@gmail.com>
2023-08-11 12:02:49 -0400

    Update mainpgm.py

--- Source code/mainpgm.py
+++ Source code/mainpgm.py
@@ -1,1 +1,21 @@
-print ("hello world')
+# this is my first waster programer
+#just added here
+import pandas as pd 
+import numpy as ny
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+    
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
+
+
+
